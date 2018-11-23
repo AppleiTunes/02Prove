01e22e4bc998a5f332c680c0a3e7709471f6ffd0
appleitunes <shanedavenport15@gmail.com>
2018-11-23 15:20:55 -0700

    Update 08Prove.R
    Added an additional data set.

--- 08Prove.R
+++ 08Prove.R
@@ -1,38 +1,19 @@
 # Include the LIBSVM package
 library(e1071)
 
-# Read in the data from the csv file
-dataSet <- read.csv("vowel.csv", head=TRUE, sep=",")
-summary(dataSet)
-
-# Partition the data into training and test sets
-# by getting a random 30% of the rows as the testRows
-index <- 1:nrow(dataSet)
-testRows <- sample(index, trunc(length(index) * 0.3))
-
-# The test set contains all the test rows
-testSet <- dataSet[testRows,]
-summary(testSet)
-
-# The training set contains all the other rows
-trainSet <- dataSet[-testRows,]
-summary(trainSet)
-
 # SVM function
-testIteration <- function(gamma, cost) {
-  # Train an SVM model
-  # Tell it the attribute to predict vs the attributes to use in the prediction,
-  #  the training data to use, and the kernal to use, along with its hyperparameters.
-  model <- svm(Speaker ~ ., data = trainSet, kernel = "radial", gamma = gamma, cost = cost)
+calc <- function(species, trainSet, testSet, gamma, cost) {
+  # Create model
+  model <- svm(species, data = trainSet, kernel = "radial", gamma = gamma, cost = cost)
   
   # For prediction with testSet excluding the Speaker column
-  prediction <- predict(model, testSet[,-1])
+  prediction <- predict(model, testSet[-1])
   
   # Produce a confusion matrix
   confusionMatrix <- table(pred = prediction, true = testSet[,1])
   
   # Calculate the accuracy, by checking the cases that the targets agreed
-  agreement <- prediction == testSet$Speaker
+  agreement <- prediction == testSet[[names(trainSet)[[1]]]]
   accuracy <- prop.table(table(agreement))
   
   # Print our results to the screen
@@ -40,21 +21,82 @@ testIteration <- function(gamma, cost) {
   print(accuracy)
 }
 
-testIteration(0.001, 1)   # true = 0.1178451
-testIteration(0.001, 10)  # true = 0.5319865 
-testIteration(0.001, 100) # true = 0.7676768 
-testIteration(0.01, 1)    # true = 0.5319865 
-testIteration(0.01, 10)   # true = 0.8148148
-testIteration(0.01, 100)  # true = 0.95286195 
-testIteration(0.1, 1)     # true = 0.8855219 
-testIteration(0.1, 10)    # true = 0.97979798
-testIteration(0.1, 100)   # true = 0.97979798
 
-# gamma = 0.5 returned the highest overall accuracy
-testIteration(0.5, 1)     # true = 0.97306397
-testIteration(0.5, 10)    # true = 0.97306397
-testIteration(0.5, 100)   # true = 0.97306397
 
-testIteration(1, 1)       # true = 0.96296296
-testIteration(1, 10)      # true = 0.96296296
-testIteration(1, 100)     # true = 0.96296296
+# Read in the data from the vowel file
+data <- read.csv("vowel.csv", head=TRUE, sep=",")
+summary(data)
+
+# Partition the data into training and test sets
+# by getting a random 30% of the rows as the testRows
+index <- 1:nrow(data)
+testRows <- sample(index, trunc(length(index) * 0.3))
+
+# The test set contains all the test rows
+testSet <- data[testRows,]
+
+# The training set contains all the other rows
+trainSet <- data[-testRows,]
+
+# Excluded column
+species = as.formula(paste(names(trainSet)[[1]], "~ ."))
+
+calc(species, trainSet, testSet, 0.001, 1)    # 0.1178451
+calc(species, trainSet, testSet, 0.001, 10)   # 0.5319865 
+calc(species, trainSet, testSet, 0.001, 100)  # 0.7676768 
+
+calc(species, trainSet, testSet, 0.01, 1)     # 0.5319865 
+calc(species, trainSet, testSet, 0.01, 10)    # 0.8148148
+calc(species, trainSet, testSet, 0.01, 100)   # 0.95286195 
+
+calc(species, trainSet, testSet, 0.1, 1)      # 0.8855219 
+calc(species, trainSet, testSet, 0.1, 10)     # 0.97979798
+calc(species, trainSet, testSet, 0.1, 100)    # 0.97979798
+
+calc(species, trainSet, testSet, 0.5, 1)      # 0.97306397
+calc(species, trainSet, testSet, 0.5, 10)     # 0.97306397
+calc(species, trainSet, testSet, 0.5, 100)    # 0.97306397
+
+calc(species, trainSet, testSet, 1, 1)        # 0.96296296
+calc(species, trainSet, testSet, 1, 10)       # 0.96296296
+calc(species, trainSet, testSet, 1, 100)      # 0.96296296
+
+
+
+# Read in the data from the letters file
+data <- read.csv("letters.csv", head=TRUE, sep=",")
+summary(data)
+
+# Partition the data into training and test sets
+# by getting a random 30% of the rows as the testRows
+index <- 1:nrow(data)
+testRows <- sample(index, trunc(length(index) * 0.3))
+
+# The test set contains all the test rows
+testSet <- data[testRows,]
+
+# The training set contains all the other rows
+trainSet <- data[-testRows,]
+
+# Excluded column
+species = as.formula(paste(names(trainSet)[[1]], "~ ."))
+
+calc(species, trainSet, testSet, 0.001, 1)    # 0.7075
+calc(species, trainSet, testSet, 0.001, 10)   # 0.8285
+calc(species, trainSet, testSet, 0.001, 100)  # 0.8706667
+
+calc(species, trainSet, testSet, 0.01, 1)     # 0.851
+calc(species, trainSet, testSet, 0.01, 10)    # 0.92
+calc(species, trainSet, testSet, 0.01, 100)   # 0.95516667
+
+calc(species, trainSet, testSet, 0.1, 1)      # 0.97333333
+calc(species, trainSet, testSet, 0.1, 10)     # 0.966
+calc(species, trainSet, testSet, 0.1, 100)    # 0.96933333
+
+calc(species, trainSet, testSet, 0.5, 1)      # 0.9695
+calc(species, trainSet, testSet, 0.5, 10)     # 0.9475
+calc(species, trainSet, testSet, 0.5, 100)    # 0.94966667
+
+calc(species, trainSet, testSet, 1, 1)        # 0.9475
+calc(species, trainSet, testSet, 1, 10)       # 0.94966667
+calc(species, trainSet, testSet, 1, 100)      # 0.94966667
