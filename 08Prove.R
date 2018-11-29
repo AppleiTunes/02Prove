076820fdf2bca5c5c77ee192b11c917f72680457
appleitunes <shanedavenport15@gmail.com>
2018-11-29 13:42:22 -0700

    Update 08Prove.R

--- 08Prove.R
+++ 08Prove.R
@@ -1,32 +1,6 @@
 # Include the LIBSVM package
 library(e1071)
 
-calcRand <- function(species, trainSet, testSet, it) {
-  for (i in 0:it) {
-    gamma = (runif(1000, 1) / 1000)[[1]]
-    cost = (sample.int(100, 1))[[1]]
-    
-    cat("Gamma = ", gamma, " Cost = ", cost)
-    
-    # Create model
-    model <- svm(species, data = trainSet, kernel = "radial", gamma = gamma, cost = cost)
-    
-    # For prediction with testSet excluding the Speaker column
-    prediction <- predict(model, testSet[-1])
-    
-    # Produce a confusion matrix
-    confusionMatrix <- table(pred = prediction, true = testSet[,1])
-    
-    # Calculate the accuracy, by checking the cases that the targets agreed
-    agreement <- prediction == testSet[[names(trainSet)[[1]]]]
-    accuracy <- prop.table(table(agreement))
-    
-    # Print our results to the screen
-    # print(confusionMatrix)
-    print(accuracy)
-  }
-}
-
 # SVM function
 calc <- function(species, trainSet, testSet, gamma, cost) {
   # Create model
@@ -67,27 +41,63 @@ trainSet <- data[-testRows,]
 # Excluded column
 species = as.formula(paste(names(trainSet)[[1]], "~ ."))
 
-# calc(species, trainSet, testSet, 0.001, 1)    # 0.1178451
-# calc(species, trainSet, testSet, 0.001, 10)   # 0.5319865 
-# calc(species, trainSet, testSet, 0.001, 100)  # 0.7676768 
+calc(species, trainSet, testSet, 0.001, 1)    # 0.1178451
+calc(species, trainSet, testSet, 0.001, 10)   # 0.5319865 
+calc(species, trainSet, testSet, 0.001, 100)  # 0.7676768 
 
-# calc(species, trainSet, testSet, 0.01, 1)     # 0.5319865 
-# calc(species, trainSet, testSet, 0.01, 10)    # 0.8148148
-# calc(species, trainSet, testSet, 0.01, 100)   # 0.95286195 
+calc(species, trainSet, testSet, 0.01, 1)     # 0.5319865 
+calc(species, trainSet, testSet, 0.01, 10)    # 0.8148148
+calc(species, trainSet, testSet, 0.01, 100)   # 0.95286195 
 
-# calc(species, trainSet, testSet, 0.1, 1)      # 0.8855219 
-# calc(species, trainSet, testSet, 0.1, 10)     # 0.97979798
-# calc(species, trainSet, testSet, 0.1, 100)    # 0.97979798
+calc(species, trainSet, testSet, 0.1, 1)      # 0.8855219 
+calc(species, trainSet, testSet, 0.1, 10)     # 0.97979798
+calc(species, trainSet, testSet, 0.1, 100)    # 0.97979798
 
-# calc(species, trainSet, testSet, 0.5, 1)      # 0.97306397
-# calc(species, trainSet, testSet, 0.5, 10)     # 0.97306397
-# calc(species, trainSet, testSet, 0.5, 100)    # 0.97306397
+calc(species, trainSet, testSet, 0.5, 1)      # 0.97306397
+calc(species, trainSet, testSet, 0.5, 10)     # 0.97306397
+calc(species, trainSet, testSet, 0.5, 100)    # 0.97306397
 
-# calc(species, trainSet, testSet, 1, 1)        # 0.96296296
-# calc(species, trainSet, testSet, 1, 10)       # 0.96296296
-# calc(species, trainSet, testSet, 1, 100)      # 0.96296296
+calc(species, trainSet, testSet, 1, 1)        # 0.96296296
+calc(species, trainSet, testSet, 1, 10)       # 0.96296296
+calc(species, trainSet, testSet, 1, 100)      # 0.96296296
 
-calcRand(species, trainSet, testSet, 10)
+# Read in the data from the letters file
+data <- read.csv("abalone.csv", head=TRUE, sep=",")
+summary(data)
+
+# Partition the data into training and test sets
+# by getting a random 30% of the rows as the testRows
+index <- 1:nrow(data)
+testRows <- sample(index, trunc(length(index) * 0.3))
+
+# The test set contains all the test rows
+testSet <- data[testRows,]
+
+# The training set contains all the other rows
+trainSet <- data[-testRows,]
+
+# Excluded column
+species = as.formula(paste(names(trainSet)[[1]], "~ ."))
+
+calc(species, trainSet, testSet, 0.001, 1)    # 0.5307263
+calc(species, trainSet, testSet, 0.001, 10)   # 0.5434956
+calc(species, trainSet, testSet, 0.001, 100)  # 0.547486
+
+calc(species, trainSet, testSet, 0.01, 1)     # 0.5450918
+calc(species, trainSet, testSet, 0.01, 10)    # 0.5442937
+calc(species, trainSet, testSet, 0.01, 100)   # 0.557063
+
+calc(species, trainSet, testSet, 0.1, 1)      # 0.5538707
+calc(species, trainSet, testSet, 0.1, 10)     # 0.575419
+calc(species, trainSet, testSet, 0.1, 100)    # 0.5682362
+
+calc(species, trainSet, testSet, 0.5, 1)      # 0.5674381
+calc(species, trainSet, testSet, 0.5, 10)     # 0.5602554
+calc(species, trainSet, testSet, 0.5, 100)    # 0.5594573
+
+calc(species, trainSet, testSet, 1, 1)        # 0.5586592
+calc(species, trainSet, testSet, 1, 10)       # 0.5442937
+calc(species, trainSet, testSet, 1, 100)      # 0.5450918
 
 # Read in the data from the letters file
 data <- read.csv("letters.csv", head=TRUE, sep=",")
@@ -107,24 +117,22 @@ trainSet <- data[-testRows,]
 # Excluded column
 species = as.formula(paste(names(trainSet)[[1]], "~ ."))
 
-# calc(species, trainSet, testSet, 0.001, 1)    # 0.7075
-# calc(species, trainSet, testSet, 0.001, 10)   # 0.8285
-# calc(species, trainSet, testSet, 0.001, 100)  # 0.8706667
+calc(species, trainSet, testSet, 0.001, 1)    # 0.7075
+calc(species, trainSet, testSet, 0.001, 10)   # 0.8285
+calc(species, trainSet, testSet, 0.001, 100)  # 0.8706667
 
-# calc(species, trainSet, testSet, 0.01, 1)     # 0.851
-# calc(species, trainSet, testSet, 0.01, 10)    # 0.92
-# calc(species, trainSet, testSet, 0.01, 100)   # 0.95516667
+calc(species, trainSet, testSet, 0.01, 1)     # 0.851
+calc(species, trainSet, testSet, 0.01, 10)    # 0.92
+calc(species, trainSet, testSet, 0.01, 100)   # 0.95516667
 
-# calc(species, trainSet, testSet, 0.1, 1)      # 0.97333333
-# calc(species, trainSet, testSet, 0.1, 10)     # 0.966
-# calc(species, trainSet, testSet, 0.1, 100)    # 0.96933333
+calc(species, trainSet, testSet, 0.1, 1)      # 0.97333333
+calc(species, trainSet, testSet, 0.1, 10)     # 0.966
+calc(species, trainSet, testSet, 0.1, 100)    # 0.96933333
 
-# calc(species, trainSet, testSet, 0.5, 1)      # 0.9695
-# calc(species, trainSet, testSet, 0.5, 10)     # 0.9475
-# calc(species, trainSet, testSet, 0.5, 100)    # 0.94966667
+calc(species, trainSet, testSet, 0.5, 1)      # 0.9695
+calc(species, trainSet, testSet, 0.5, 10)     # 0.9475
+calc(species, trainSet, testSet, 0.5, 100)    # 0.94966667
 
-# calc(species, trainSet, testSet, 1, 1)        # 0.9475
-# calc(species, trainSet, testSet, 1, 10)       # 0.94966667
-# calc(species, trainSet, testSet, 1, 100)      # 0.94966667
-
-calcRand(species, trainSet, testSet, 10)
+calc(species, trainSet, testSet, 1, 1)        # 0.9475
+calc(species, trainSet, testSet, 1, 10)       # 0.94966667
+calc(species, trainSet, testSet, 1, 100)      # 0.94966667
